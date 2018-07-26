081c667eddfb929354132508ef6763274f658583
Timothy Mickle <timothyrmickle@gmail.com>
2018-07-26 17:42:54 -0500

    attempting to load to heroku

--- app.js
+++ app.js
@@ -16,15 +16,15 @@ const indexController = require('./controllers/index');
 const photosController = require('./controllers/photos');
 const homeController = require('./controllers/home');
 
-var MongoClient = require('mongodb').MongoClient;
+// var MongoClient = require('mongodb').MongoClient;
 
 
-var uri = "mongodb+srv://user:FouFouLove1@cluster0-apmqh.gcp.mongodb.net/test?retryWrites=true";
-MongoClient.connect(uri, function(err, client) {
-   const collection = client.db("test").collection("devices");
-   // perform actions on the collection object
-   client.close();
-});
+// var uri = "mongodb+srv://user:FouFouLove1@cluster0-apmqh.gcp.mongodb.net/test?retryWrites=true";
+// MongoClient.connect(uri, function(err, client) {
+//    const collection = client.db("test").collection("devices");
+//    // perform actions on the collection object
+//    client.close();
+// });
 
 // set up CSS
 app.use(express.static('public'));
